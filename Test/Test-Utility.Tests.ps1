7b7b248a5061f96c24fec35a731ecce16a31bea5
Steffen Kampmann <steffen.kampmann@gmail.com>
2022-03-13 22:55:15 +0100

    added test-utility

---
+++ Test/Test-Utility.Tests.ps1
@@ -0,0 +1,13 @@
+#Requires -Modules @{ ModuleName='Pester'; ModuleVersion='5.0.0' }
+
+Describe 'Test-Utility' {
+
+    BeforeAll {
+        Import-Module $PSScriptRoot\..\Source\PsSqlLocalDb.psd1 -Force -ErrorAction Stop
+    }
+
+    It 'works' {
+        Test-LocalDbUtility | Should -BeIn $true, $false
+    }
+
+}
