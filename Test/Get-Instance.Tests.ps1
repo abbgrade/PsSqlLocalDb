4370935fd4d13ec80914afcc9e69889b584bd9ff
Steffen Kampmann <steffen.kampmann@gmail.com>
2022-03-27 19:51:28 +0200

    guarantee at least one instance

--- Test/Get-Instance.Tests.ps1
+++ Test/Get-Instance.Tests.ps1
@@ -6,29 +6,39 @@ Describe 'Get-Instance' {
         Import-Module $PSScriptRoot\..\Source\PsSqlLocalDb.psd1 -Force -ErrorAction Stop
     }
 
-    Context 'LocalDb' -Skip:( -Not ( Test-LocalDbUtility )) {
-
-        It 'Returns values' {
-            $result = Get-LocalDbInstance
-
-            $result | Should -Not -BeNullOrEmpty
-            $result.Name | Should -Not -BeNullOrEmpty
-            $result.Version | Should -Not -BeNullOrEmpty
+    Context 'First Instance' {
+        BeforeAll {
+            $Script:FirstInstance = New-LocalDbInstance
         }
 
-        Context 'New Instance' {
-            BeforeAll {
-                $Script:Instance = New-LocalDbInstance
+        AfterAll {
+            $Script:FirstInstance | Remove-LocalDbInstance
+        }
+
+        Context 'LocalDb' -Skip:( -Not ( Test-LocalDbUtility )) {
+
+            It 'Returns values' {
+                $result = Get-LocalDbInstance
+
+                $result | Should -Not -BeNullOrEmpty
+                $result.Name | Should -Not -BeNullOrEmpty
+                $result.Version | Should -Not -BeNullOrEmpty
             }
 
-            AfterAll {
-                $Script:Instance | Remove-LocalDbInstance
-            }
+            Context 'New Instance' {
+                BeforeAll {
+                    $Script:Instance = New-LocalDbInstance
+                }
 
-            It 'Returns a specific instance' {
-                $result = Get-LocalDbInstance -Name $Script:Instance.Name
-                $result.Count | Should -Be 1
-                $result.Name | Should -Be $Script:Instance.Name
+                AfterAll {
+                    $Script:Instance | Remove-LocalDbInstance
+                }
+
+                It 'Returns a specific instance' {
+                    $result = Get-LocalDbInstance -Name $Script:Instance.Name
+                    $result.Count | Should -Be 1
+                    $result.Name | Should -Be $Script:Instance.Name
+                }
             }
         }
     }
